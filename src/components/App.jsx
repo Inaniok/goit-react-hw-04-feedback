35f2d451e3823927e240c5e70865958a1daf61ac
Serhii Izotov <sergiyizotov@gmail.com>
2023-05-10 17:16:52 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,15 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
 import { Wrapper } from './App.styled';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  
   handleFeedbackBtnClick = feedback => {
     this.setState(prevState => ({
       [feedback]: prevState[feedback] + 1,
@@ -31,7 +29,7 @@ export class App extends Component {
     const { good, neutral, bad } = this.state;
     const total = this.countTotalFeedback();
     const positivePercentage = this.countPositiveFeedbackPercentage();
-
+  
     return (
       <Wrapper>
         <Section title="Please leave feedback">
